926cda8726a0db3e5a8b2d3764d0c9c24a8b83b8
Ismail Degani <idegani@mit.edu>
2017-07-18 16:44:43 -0400

    Push test images

---
+++ src/recon.py
@@ -0,0 +1,25 @@
+class Reconstruction(object):
+    lambda = 405
+    UpsampleFactor = 2
+           delta
+           Threshold_objsupp = 0.09
+
+
+
+
+    def compute(self, srcImage):
+        k =
+
+
+
+
+
+
+
+
+
+
+recon = new Reconstruction()
+recon.wavelength = 405
+recon.delta = 2.2e-6
+result = recon.compute(srcImage)
