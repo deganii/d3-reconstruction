2aef04103162efd0a1e961b58e09324cb15d09c6
Ismail Degani <idegani@mit.edu>
2017-07-18 19:38:36 -0400

    Code runs (still need to check correctness)

--- src/recon.py
+++ src/recon.py
@@ -1,6 +1,10 @@
 import numpy as np
 import scipy
+import scipy.misc
+import scipy.ndimage
+
 import skimage
+import skimage.morphology
 
 class Reconstruction(object):
     def __init__(self):
@@ -8,38 +12,31 @@ class Reconstruction(object):
         # lambda is the wavelength in meters (i.e. 405nm = UV light)
         self.lmbda = 405e-9
         self.UpsampleFactor = 2
-        self.delta = 2.2e-6
+        self.delta2 = 2.2e-6
         self.Dz = 5e-4
         self.Threshold_objsupp = 0.09
         self.NumIteration = 30
 
     def upsampling(self, data, dx1):
-        # TODO: fix interpolation
-        #upsampled = scipy.interpolate.interp2d(data, self.)
-        # scipy.interpolate.griddata
         dx2 = dx1/(2 ** self.UpsampleFactor)
-
-
-        upsampled = data
-        dx2 = 1
-
+        # bicubic resample (order = 3)
+        upsampled = scipy.ndimage.zoom(data, self.UpsampleFactor, order=3)
         return upsampled, dx2
 
     def ft2(self, g, delta):
-        return np.fftshift(np.fft2((g))) * delta**2
+        return np.fft.fftshift(np.fft.fft2((g))) * delta**2
 
     def ift2(self, G, dfx, dfy):
         Nx, Ny = np.shape(G)
-        return np.ifftshift(np.ifft2(np.ifftshift(G))) * Nx*Ny*dfx*dfy;
-
+        return np.fft.ifftshift(np.fft.ifft2(np.fft.ifftshift(G))) * Nx*Ny*dfx*dfy
 
     def compute(self, data):
         k = 2*np.pi/self.lmbda
         subNormAmp = np.sqrt(data)
-        delta_prev = self.delta2
+        # delta_prev = self.delta2
 
         if self.UpsampleFactor > 0:
-            subNormAmp,self.delta2 = self.upsampling(subNormAmp);
+            subNormAmp, self.delta2 = self.upsampling(subNormAmp, self.delta2)
 
         Nx, Ny = np.shape(subNormAmp)
         delta1 = self.delta2
@@ -47,12 +44,11 @@ class Reconstruction(object):
         dfx = 1 / (Nx * self.delta2)
         dfy = 1 / (Ny * self.delta2)
 
-        fx, fy = np.meshgrid(np.arange(-Ny / 2, Ny / 2 - 1, dfy),
-                             np.arange(-Nx / 2, Nx / 2 - 1, dfx))
+        fx, fy = np.meshgrid(np.arange(-Ny / 2, Ny / 2 - 1, 1) * dfy,
+                             np.arange(-Nx / 2, Nx / 2 - 1, 1) * dfx)
 
-        Gbp = np.zeros(Nx, Ny)
-        Gfp = np.zeros(Nx, Ny)
-
+        Gbp = np.zeros(shape=(Nx, Ny), dtype=np.complex_)
+        Gfp = np.zeros(shape=(Nx, Ny), dtype=np.complex_)
 
         for n in range(len(fx)):
             for m in range(len(fx[0])):
@@ -76,7 +72,7 @@ class Reconstruction(object):
                 # scipy.ndimage.morphology.binary_opening(support, min_size=64, connectivity=2)
                 skimage.morphology.remove_small_objects(support, min_size=64, connectivity=2)
 
-            Constraint = np.ones(Recon1.shape);
+            Constraint = np.ones(Recon1.shape)
             for p in range(Recon1.shape[0]):
                 for q in range(Recon1.shape[1]):
                     if support[p, q] == 1:
@@ -91,15 +87,24 @@ class Reconstruction(object):
 
             Output = self.ift2(np.multiply(F1, Gfp), dfx, dfy)
 
-            Input =np.multiply(subNormAmp, np.exp(1j * np.angle(Output)))
+            Input = np.multiply(subNormAmp, np.exp(1j * np.angle(Output)))
 
+            print("Completing Iteration {0} of {1}  -  {2}%".format(k, self.NumIteration, 100. * k / self.NumIteration))
             if k == self.NumIteration:
                 return Input
+
+    def process(self, image_path, reference_path):
+        image = np.array(scipy.misc.imread(image_path))
+        ref = np.array(scipy.misc.imread(reference_path))
+        norm_factor = np.mean(ref) / np.mean(image)
+        data = np.divide(image, ref) * norm_factor
+        return self.compute(data)
+
 
 
 
 # Usage
-# recon = Reconstruction()
-# recon.lmbda = 405
+recon = Reconstruction()
+# recon.lmbda = 405e-9
 # recon.delta = 2.2e-6
-# result = recon.compute(srcImage)
+result = recon.process('../test/Daudi_Kconcentrated.png', '../test/reference_image.png', )
