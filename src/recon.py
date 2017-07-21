a353c3d0947c91269717cd4f046458556b7e2061
Matthew Allen <AechPro@users.noreply.github.com>
2017-07-20 23:06:09 -0400

    Update recon.py
    CHANGELOG:
    All changes done inside Reconstruction.compute().
    - Removed debug calls inside for loop (saves time on function calls).
    - Assigned commonly used functions as local variables (faster access rate).
    - Moved support code to before for loop (saves time on if statement, seemed to drastically increase performance?).
    - Moved F2 and Recon1 calculations to bottom of for loop to allow support code to work outside of loop without loss in performance.

--- src/recon.py
+++ src/recon.py
@@ -7,8 +7,10 @@ import scipy.interpolate
 import skimage
 import time
 import cv2
+import os.path
 import skimage.morphology
 from scipy.ndimage.filters import uniform_filter
+
 
 class Reconstruction(object):
     def __init__(self):
@@ -20,47 +22,42 @@ class Reconstruction(object):
         self.Dz = 5e-4
         self.Threshold_objsupp = 0.09
         self.NumIteration = 30
+        self.std_filter_size = 9
+        self.dilation_size = 6
+        self.debug = False
 
     def upsampling(self, data, dx1):
-        dx2 = dx1/(2 ** self.UpsampleFactor)
-
-        # bicubic resample (order = 3)
-        # upsampled = scipy.ndimage.zoom(data, 2 ** self.UpsampleFactor, order=3)
-        # x = np.arange(data.shape[0])
-        # y = np.arange(data.shape[1])
-        # f = scipy.interpolate.interp2d(x, y, data, kind='cubic')
-
-        # x_new = np.linspace(0, data.shape[0], x_size, endpoint=True)
-        # y_new = np.linspace(0, data.shape[0], y_size, endpoint=True)
-
-        # x_new = np.arange(0, data.shape[0], 1 / 2 ** self.UpsampleFactor)
-        # y_new = np.arange(0, data.shape[1], 1 / 2 ** self.UpsampleFactor)
-        # upsampled = f(y_new, x_new)
-
-        x_size = data.shape[0]
-        y_size = data.shape[1]
-        for i in range(self.UpsampleFactor):
-            x_size = (x_size * 2) - 1
-            y_size = (y_size * 2) - 1
-
-        upsampled = scipy.ndimage.zoom(data, [x_size/data.shape[0], y_size/data.shape[1]], order=3)
-
+        dx2 = dx1 / (2 ** self.UpsampleFactor)
+        x_size = ((2 ** self.UpsampleFactor) * data.shape[0]) - (2 ** (self.UpsampleFactor) - 1)
+        y_size = ((2 ** self.UpsampleFactor) * data.shape[1]) - (2 ** (self.UpsampleFactor) - 1)
+        upsampled = scipy.ndimage.zoom(data, [x_size / data.shape[0], y_size / data.shape[1]], order=3)
         self.debug_save_mat(upsampled, 'upsampledPy')
-
         return upsampled, dx2
 
     def ft2(self, g, delta):
-        return np.fft.fftshift(np.fft.fft2((g))) * delta**2
+        return np.fft.fftshift(np.fft.fft2((g))) * delta ** 2
 
     def ift2(self, G, dfx, dfy):
         Nx, Ny = np.shape(G)
-        return np.fft.ifftshift(np.fft.ifft2(np.fft.ifftshift(G))) * Nx*Ny*dfx*dfy
+        return np.fft.ifftshift(np.fft.ifft2(np.fft.ifftshift(G))) * Nx * Ny * dfx * dfy
 
     def debug_save_mat(self, matrix, mname):
-        scipy.io.savemat(mname + '.mat', {mname: matrix})
+        if self.debug:
+            if matrix is None:
+                matrix = np.array([0])
+
+            file_path = '../debug/' + mname + '.mat'
+            directory = os.path.dirname(file_path)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            scipy.io.savemat(file_path, {mname: matrix})
 
     def compute(self, data):
-        k = 2*np.pi/self.lmbda
+        ft2 = self.ft2
+        ift2 = self.ift2
+        mul = np.multiply
+        k = 2 * np.pi / self.lmbda
         subNormAmp = np.sqrt(data)
         # delta_prev = self.delta2
 
@@ -77,48 +74,46 @@ class Reconstruction(object):
 
         fx, fy = np.meshgrid(np.arange(-Ny / 2, Ny / 2, 1) * dfy,
                              np.arange(-Nx / 2, Nx / 2, 1) * dfx)
-        Gbp = np.zeros(shape=(Nx, Ny), dtype=np.complex_)
-        Gfp = np.zeros(shape=(Nx, Ny), dtype=np.complex_)
-        Gbp = np.exp(1j * k * self.Dz * np.sqrt(1 - self.lmbda ** 2 * fx ** 2 - self.lmbda ** 2 * fy ** 2))
-        Gfp = np.exp(-1j * k * self.Dz * np.sqrt(1 - self.lmbda ** 2 * fx ** 2 - self.lmbda ** 2 * fy ** 2))
-        
+
+        Gbp = np.exp((1j * k * self.Dz) * np.sqrt(1 - self.lmbda ** 2 * fx ** 2 - self.lmbda ** 2 * fy ** 2))
+        Gfp = np.exp((-1j * k * self.Dz) * np.sqrt(1 - self.lmbda ** 2 * fx ** 2 - self.lmbda ** 2 * fy ** 2))
+
         self.debug_save_mat(Gbp, 'GbpPy')
         Input = subNormAmp
-
+        
+        F2 = ft2(Input, self.delta2)
+        Recon1 = ift2(mul(F2, Gbp), dfx, dfy)
+        
+        support = self.window_stdev(np.abs(np.real(Recon1)), self.std_filter_size / 2)
+        self.debug_save_mat(support, 'supportStdPy')
+        
+        support = np.where(support > self.Threshold_objsupp, 1, 0)
+        self.debug_save_mat(support, 'supportThresholdPy')
+        
+        support = scipy.ndimage.binary_dilation(support, structure=skimage.morphology.disk(self.dilation_size))
+        self.debug_save_mat(support, 'supportDilatePy')
         for k in range(self.NumIteration):
-            F2 = self.ft2(Input, self.delta2);
-            Recon1 = self.ift2(np.multiply(F2, Gbp), dfx, dfy);
-            self.debug_save_mat(Recon1, 'Recon1Py')
-            if k == 0:
-                # abs(Recon1).*cos(angle(Recon1) == abs(real(Recon1)
-                support = scipy.ndimage.filters.generic_filter(np.abs(np.real(Recon1)), function=np.std, size=(9, 9))
-                #support = self.window_stdev(np.abs(np.real(Recon1)),4.5)
-                self.debug_save_mat(support, 'supportStdPy')
-                support = np.where(support > self.Threshold_objsupp, 1, 0)
-                self.debug_save_mat(support, 'supportThresholdPy')
-                support = scipy.ndimage.binary_dilation(support, structure=skimage.morphology.disk(6))
-                self.debug_save_mat(support, 'supportDilatePy')
-                # TODO Fix hole-filling and bwareaopen replacements
-                #segmentation = scipy.ndimage.binary_fill_holes(segmentation - 1)
-                # scipy.ndimage.morphology.binary_opening(support, min_size=64, connectivity=2)
-                #support = skimage.morphology.remove_small_objects(support, min_size=64, connectivity=2)
             Constraint = np.ones(Recon1.shape)
-            Constraint = np.where(support==1,np.abs(Recon1),1)
-            Constraint = np.where(np.abs(Recon1)>1,1,Constraint)
-            self.debug_save_mat(Constraint, 'ConstraintPy')
-            Recon1_update =np.multiply(Constraint,  np.exp(1j * np.angle(Recon1)))
-
-            F1 = self.ft2(Recon1_update, delta1)
-
-            Output = self.ift2(np.multiply(F1, Gfp), dfx, dfy)
-
-            Input = np.multiply(subNormAmp, np.exp(1j * np.angle(Output)))
-
-            print("Completing Iteration {0} of {1}  -  {2:.2f}%".format(k, self.NumIteration, 100. * k / self.NumIteration))
-
-        F2 = self.ft2(Input, self.delta2)
-        ReconImage = self.ift2(np.multiply(F2, Gbp), dfx, dfy)
-        self.debug_save_mat(ReconImage, 'ReconImagePy')
+            Constraint = np.where(support == 1, np.abs(Recon1), 1)
+            Constraint = np.where(np.abs(Recon1) > 1, 1, Constraint)
+            
+            Recon1_update = mul(Constraint, np.exp(1j * np.angle(Recon1)))
+            
+            F1 = ft2(Recon1_update, delta1)
+            
+            Output = ift2(mul(F1, Gfp), dfx, dfy)
+            
+            Input = mul(subNormAmp, np.exp(1j * np.angle(Output)))
+            
+            F2 = ft2(Input, self.delta2)
+            
+            Recon1 = ift2(mul(F2, Gbp), dfx, dfy)
+            print("Completing Iteration {0} of {1}  -  {2:.2f}%".format(k, self.NumIteration,
+                                                                        100.*k / self.NumIteration))
+        
+        F2 = ft2(Input, self.delta2)
+        ReconImage = ift2(mul(F2, Gbp), dfx, dfy)
+        
         return ReconImage
 
     def process(self, image_path, reference_path):
@@ -127,14 +122,30 @@ class Reconstruction(object):
         norm_factor = np.mean(ref) / np.mean(image)
         data = np.divide(image, ref) * norm_factor
         return self.compute(data)
-    
+
     def window_stdev(self, arr, radius):
-        diameter = int(round(radius*2))
-        c1 = uniform_filter(arr, diameter, mode='constant', origin=-int(round(radius)))
-        c2 = uniform_filter(arr*arr, diameter, mode='constant', origin=-int(round(radius)))
-        return ((c2 - c1*c1)**.5)[:-diameter+1,:-diameter+1]
-
-
+        diameter = int(round(radius * 2))
+        radius = int(round(radius))
+        c1 = uniform_filter(arr, diameter, mode='constant', origin=-radius)
+        c2 = uniform_filter(arr * arr, diameter, mode='constant', origin=-radius)
+        c_std = ((c2 - c1 * c1) ** .5)[:-diameter + 1, :-diameter + 1]
+        # symmetric padding to match Matlab stdfilt:
+        return np.pad(c_std, radius, 'symmetric')
+   
+    def hough_circle_transform(self, support):
+        support8bit = np.array(support * 255, dtype=np.uint8)
+        circles = cv2.HoughCircles(support8bit, cv2.HOUGH_GRADIENT,
+                                   1, 20, param1=50, param2=30, minRadius=0, maxRadius=6)
+        self.debug_save_mat(circles, 'circlesPy')
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                # draw the outer circle
+                cv2.circle(support8bit, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                # draw the center of the circle
+                cv2.circle(support8bit, (i[0], i[1]), 2, (0, 0, 255), 3)
+        self.debug_save_mat(support8bit, 'supportCirclesPy')
+        return support8bit
 
 
 # Usage
@@ -142,5 +153,8 @@ recon = Reconstruction()
 # change parameters if needed
 # recon.lmbda = 405e-9
 # recon.delta = 2.2e-6
+recon.debug = True
+t1 = time.time()
 result = recon.process('test image.png', 'ref.png')
+print("Reconstruction runs in:", time.time() - t1,"seconds")
 scipy.misc.imsave('output.png', np.abs(result))
